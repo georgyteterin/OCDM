bf47106cb6409afd50007b50a9fc783e4c3ac6cf
georgyteterin <teterin.normal@gmail.com>
2024-11-16 14:50:56 +0300

    найдена взаимосвязь с fft, сравнение трех способов преобразования Френеля

--- имитационная модель/three_steps.m
+++ имитационная модель/three_steps.m
@@ -1,6 +1,6 @@
 clear, clc
 
-N = 2^2;
+N = 2^10;
 input = randi([0 1], N, 1);
 iniPh = exp(-1j*(pi/4));
 sqrtN = sqrt(N);
@@ -41,40 +41,28 @@ end
 dftMat = zeros(N);
 for m=1:N
     for n=1:N
-        dftMat(n, m) = (1/sqrtN)*exp(-1j*(2*pi/N)*m*n);
+        dftMat(n, m) = (1/sqrtN)*exp(-1j*(2*pi/N)*(m - 1)*(n - 1));
     end
 end
 
 %%
 % матрицы
-out1 = DFnTmtrx(input);
-out2 = Teta1*dftMat*Teta2;
+
+out1 = input'*DFnTmtrx(input);
+
+out2 = input'*Teta1*dftMat*Teta2;
 
 % преобразование
 
-output1 = DFnTmtrx(input)*input;
-tmp1 = input'*Teta1;
-tmp2 = tmp1*dftMat;
-output2 = tmp2*Teta2;
-% output2 = Teta1*dftMat*Teta2*input;
+tic;output1 = DFnT(input);toc;
 
-%% без матриц
+tic;
+tmp11 = Teta1*input;
+tmp12 = dftMat*tmp11;
+output2 = Teta2*tmp12;toc;
 
-for m=1:N
-    if mod(N, 2) == 0
-        tmp1(m) = input(m)*iniPh*exp(1j*(pi/N)*m^2);
-    else
-        tmp1(m) = input(m)*iniPh*exp(1j*(pi/(4*N)))*exp(1j*(pi/N)*(m^2 + m));
-    end
-end
-
-tmp2 = (1/sqrtN)*(fftshift(fft(tmp1)));
-
-%%
-
-f1 = (1/sqrtN)*(dftmtx(N));
-f2 = dftMat;
-
-z = 1:N;
-o1 = z*f2;
-o2 = sqrtN*ifft(z, N);
+tic;
+tmp21 = Teta1*input;
+tmp22 = fft(tmp21, N);
+tmp22 = (1/sqrtN)*tmp22;
+output3 = Teta2*tmp22;toc;
