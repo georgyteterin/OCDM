8a78c691966bc71bcd108b097d0a54b1868ab868
georgyteterin <teterin.normal@gmail.com>
2024-12-06 03:27:01 +0300

    обновленная модель, хорошо работает в АБГШ, в канале с замираниями: в процессе

--- имитационная модель/OFDM_EVAChannel.m
+++ имитационная модель/OFDM_EVAChannel.m
@@ -1,43 +1,37 @@
-
 clc; clear; close all
 
 
 % Параметры
-    params.NFFT = 2^6;                     % размер сообщения/NFFT
-    params.NumSubcar = 64;               % Число поднесущих "пилот + данные"
-    params.NumData = 64-2;
-    params.NumPilots = 4;
+    params.NFFT = 2^8;                     % размер сообщения/NFFT
+    params.NumSubcar = 52;               % Число поднесущих "пилот + данные"
+    params.NumPilots = 6;
+    params.NumData = params.NumSubcar - params.NumPilots;
     params.T = 4e-6;                        % длительность символа включая префикс в с
     params.cpDur = 0.8e-6;                 % длительность префикса в с    
     params.M = 16;                          % Размер созвездия для информационных поднесущих
-    h2dB = 1:30;
+    params.Mp = 4;
+    h2dB = -2:30;
     BER = zeros(size(h2dB));
-    NumFrame = 50;
+    NumFrame = 200;
 
+
+
+    tmp_sig = [];
 % Задания
-    eq = 0;
-    cPref = 0;
-    cCycle = 0;
-    rayCh = 0;
-    dft = 1;
+    eq = 1;
+    prefix = 'CP'; % 'CP'/'ZP'
+    rayCh = 1;
 % Вычисляемые параметры
     log2M = log2(params.M);
-
-    params.Fs = (5/4)*params.NFFT/params.T;       % частота дискретизации
-    params.cpLen = params.cpDur * params.Fs;
-    params.left = (params.NFFT - params.NumData) / 2;       % Размер защитного интервала слева
-    params.right = (params.NFFT - params.NumData) / 2;      % Размер защитного интервала справа
-    params.infBitsNum = (params.NumData - 1)*log2M;
-
-    % матрицы
-    dftMat = zeros(params.NFFT);
-    for m=1:params.NFFT
-        for n=1:params.NFFT
-            dftMat(n, m) = exp(-1j*(2*pi/params.NFFT)*(m - 1)*(n - 1));
-        end
-    end
+    log2Mp = log2(params.Mp);
+    dM = log2M/log2Mp;
     
-    idftMat = (1/params.NFFT)*dftMat';
+    params.pilotStep = floor(params.NumSubcar/(params.NumPilots-1));
+    params.Fs = params.NFFT/params.T;       % частота дискретизации
+    params.cpLen = 0.25*params.NFFT;
+    params.left = (params.NFFT - params.NumSubcar) / 2;       % Размер защитного интервала слева
+    params.right = (params.NFFT - params.NumSubcar) / 2;      % Размер защитного интервала справа
+    params.infBitsNum = (params.NumData - 1)*log2M;
 
 %% Передающая часть 
 % Вычисление индексов информац-х, пилотных и защитных поднесущих
@@ -46,8 +40,12 @@ clc; clear; close all
     ind.right = ((params.NFFT - params.right + 1) : params.NFFT).';
 
     % Индексы пилотных поднесущих
-%     ind.pilots = [1 14 28 40] + ind.left(end);
-    ind.pilots = [];
+    ind.pilots(1) = 1;
+    for i=2:params.NumPilots-1
+        ind.pilots(i) = ind.pilots(i-1)+params.pilotStep;
+    end
+    ind.pilots(params.NumPilots) = params.NumSubcar;
+    ind.pilots = ind.pilots + ind.left(end);
 
     % Индекс главной поднесущей
     ind.mainCarrier = params.NFFT/2 + 1;
@@ -74,8 +72,8 @@ clc; clear; close all
     clear tmplogic i j;
     
 
-for SNR = 1:max(h2dB)
-% for SNR = 20
+for s = 1:length(h2dB)
+    SNR = h2dB(s);
     EbN0 = convertSNR(SNR,'ebno',BitsPerSymbol=log2M);
     for Frame = 1:NumFrame
     % Формирование OFDM сигнала
@@ -92,14 +90,15 @@ for SNR = 1:max(h2dB)
     
     % Формировнаие мод-нных символов информационных поднесущих
         tmp = reshape(txBitsExtended, (params.NumData - 1)*log2M, []);
-        txSymbols = qammod(tmp, params.M, 'gray', 'InputType', 'bit');
+        txSymbols = qammod(tmp, params.M, 'gray', 'UnitAveragePower', true, 'InputType', 'bit');
         constInt = 0:params.M - 1;
         constellation = qammod(constInt, params.M);
-        Es = mean(abs(constellation).^2);
-        Eb = Es/log2M;
+%         Es = mean(abs(constellation).^2);
+%         Eb = Es/log2M;    
+        Eb = 1/params.NFFT/log2M;
         clear tmp;
     % Формировнаие мод-нных символов пилотных поднесущих
-        pilotSymbs = qammod(mod(0:params.NumPilots-1, 16).', 16, 'gray', 'UnitAveragePower', false, 'InputType', 'integer');
+        pilotSymbs = qammod(mod(0:params.NumPilots-1, 4).', params.Mp, 'gray', 'UnitAveragePower', true, 'InputType', 'integer');
 %         pilotSymbs = 3+3j;
     
     % Заготовка формирования OFDM сигнала
@@ -109,77 +108,60 @@ for SNR = 1:max(h2dB)
         % Цикл по символам
         for i = 1 : size(txMatrix, 2)
             % Заполнение пилотами
-%             txMatrix(ind.pilots, i) = pilotSymbs;
+            txMatrix(ind.pilots, i) = pilotSymbs;
     
             % Заполнение информацией
             txMatrix(ind.data, i) = txSymbols(:, i);
         end
     
-    % Обратное дискретное преобразование Фурье
- %         txSignal = txMatrix;
-     if dft
-        txSignal = ifft(ifftshift(txMatrix), params.NFFT);
-     else
-        txSignal = idftMat*txMatrix;
-     end
-    %     txSignal = IDFnT(txMatrix);
+    % Обратное дискретное преобразование Фурьe
+    txSignal = ifft(ifftshift(txMatrix), params.NFFT);
         
+    txSignalWithPrefix = zeros(params.cpLen + params.NFFT, size(txSignal, 2));
     % Защитный интервал 
-     if(cPref == 1)
-         txSignalWithCp = zeros(params.cpLen + params.NFFT, size(txSignal, 2));
-         if(cCycle == 1)
-            txSignalWithCp(1 : params.cpLen, :) = txSignal(end - params.cpLen + 1 : end, :);
-         end
-         txSignalWithCp(params.cpLen + 1 : end, :) = txSignal;
-    
-    % Передаваемый сигнал
-        txSignal = txSignalWithCp(:);
-    else
-        txSignal = txSignal(:);
-    end
-    clear txSignalWithCp;
+     if(prefix == "CP")
+        txSignalWithPrefix(1 : params.cpLen, :) = txSignal(end - params.cpLen + 1 : end, :);
+     end
+     txSignalWithPrefix(params.cpLen + 1 : end, :) = txSignal;
+     txSignal = txSignalWithPrefix;
+
+    clear txSignalWithPrefix;
     
     % Канал связи
         % Релеевский канал (EVA)
         if(rayCh == 1)
-            EVA.PDelaysNs = [0, 30, 150, 310, 370, 710, 1090, 1730, 2510] * 1e-9;
-            EVA.APGdB = [0, -1.5, -1.4, -3.6, -0.6, -9.1, -7.0, -12, -16.9];
+            EVA.PDelaysNs = [0, 30] * 1e-9;
+%             EVA.APGdB = [0, -1.5, -1.4, -3.6, -0.6, -9.1, -7.0, -12, -16.9];
+            EVA.APGdB = [0,-10];
             EVA.MDSHz = 0;
     %         rayleighChan = comm.RayleighChannel("SampleRate", Fs, "PathDelays", EVA.PDelaysNs, "AveragePathGains", EVA.APGdB, "MaximumDopplerShift", EVA.MDSHz,'Visualization','Impulse and frequency responses');
             rayleighChan = comm.RayleighChannel("SampleRate", params.Fs, "PathDelays", EVA.PDelaysNs, "AveragePathGains", EVA.APGdB, "MaximumDopplerShift", EVA.MDSHz);
-            rxSignal = rayleighChan(txSignal);
+            rxSignal = rayleighChan([txSignal; zeros(10,1)]);
             rayleighChan.release();
-            rxSignalBeforeAWGN = rxSignal;
+            rxSignalBeforeAWGN = rxSignal(8:8+params.NFFT+params.cpLen-1);
+            tmp_srig = [tmp_sig; rxSignalBeforeAWGN];
         else
             rxSignalBeforeAWGN = txSignal;
         end
         % АБГШ
-        h2 = 10^(h2dB(SNR)/10);
-        No = Eb/h2;
-        noise = sqrt(1/2 * randn(length(rxSignalBeforeAWGN), 2) * 2 * No) * [1; 1j];
+%         h2 = 10^(h2dB(SNR)/10);
+%         No = Eb/h2;
+%         noise = sqrt(1/2 * randn(length(rxSignalBeforeAWGN), 2) * 2 * No) * [1; 1j];
+%         
         
-        
-%         N0 = Eb/10^(SNR/10);
-%         noise = (randn(size(rxSignalBeforeAWGN))+1j*randn(size(rxSignalBeforeAWGN))).*sqrt(N0/2);
-%         rxSignal = rxSignalBeforeAWGN + noise;
-        rxSignal = awgn(rxSignalBeforeAWGN, EbN0, 'measured');
+        N0 = Eb/10^(SNR/10);
+        noise = normrnd(0,sqrt(N0/2),length(rxSignalBeforeAWGN),2) * [1; 1i];
+        rxSignal = rxSignalBeforeAWGN + noise;
+%         rxSignal = awgn(rxSignalBeforeAWGN, EbN0, 'measured');
       
     % Приёмная часть
     % Обработка принятого сигнала
         % Разбиение по кадрам
-        if (cPref == 0)
-            rxMatrix = reshape(rxSignal, params.NFFT, []);
-        else
-            rxMatrix = reshape(rxSignal, params.NFFT + params.cpLen, []);
-            rxMatrix = rxMatrix(params.cpLen + 1 : end, :);
-        end
+        rxMatrix = reshape(rxSignal, params.NFFT + params.cpLen, []);
+        rxMatrix = rxMatrix(params.cpLen + 1 : end, :);
         % Прямое ДПФ
-        if dft
-            rxMatrix = fftshift(fft(rxMatrix,  params.NFFT));
-        else
-            rxMatrix = dftMat*rxMatrix;
-        end
-    %     rxMatrix = DFnT(rxMatrix);
+        rxMatrix = fftshift(fft(rxMatrix,  params.NFFT));
+
     
     % Извлечение символов
         % Заготовка под пилоты
@@ -191,10 +173,10 @@ for SNR = 1:max(h2dB)
         % Цикл по символам 
         for i = 1 : size(rxMatrix, 2)
             % Извлечение мод-нных символов
-                rxSymbols(:, i) = rxMatrix(ind.data, i);
+            rxSymbols(:, i) = rxMatrix(ind.data, i);
     
             % Извлечение пилотов
-%             rxPilots(:, i) = rxMatrix(ind.pilots, i);
+            rxPilots(:, i) = rxMatrix(ind.pilots, i);
         end
     
     % Эквалайзер
@@ -206,13 +188,13 @@ for SNR = 1:max(h2dB)
         rxSymbols = rxSymbols ./ Heq;
     end
     % Демодуляция
-        rxBitsExtended = qamdemod(rxSymbols, params.M, 'gray', 'OutputType', 'bit');
+        rxBitsExtended = qamdemod(rxSymbols, params.M, 'gray', 'UnitAveragePower', true, 'OutputType', 'bit');
         rxBitsExtended = rxBitsExtended(:);
     
         rxBits= rxBitsExtended(1:length(txBits));
     
     % Вычисление BER
-        BER(SNR) = BER(SNR) + sum((txBits ~= rxBits));
+        BER(s) = BER(s) + sum((txBits ~= rxBits));
     end
 end
 
@@ -224,6 +206,6 @@ BERth = berawgn(h2dB, "qam", params.M);
 semilogy(h2dB, BER);
 grid on;
 hold on;
-semilogy(BERth);
+semilogy(h2dB, BERth, '--');
 ylim([10^-6 1]);
 % title("BER comparison");
